8139076113a46fcc4c42b02f8e975fee5b6f3ed6
Kevin Bond <kevinbond@gmail.com>
2021-10-28 14:49:59 -0400

    fixcs

--- src/SignedUrl/Attribute/Signed.php
+++ src/SignedUrl/Attribute/Signed.php
@@ -5,7 +5,7 @@ namespace Zenstruck\SignedUrl\Attribute;
 /**
  * @author Kevin Bond <kevinbond@gmail.com>
  */
-#[\Attribute(\Attribute::TARGET_CLASS|\Attribute::TARGET_METHOD)]
+#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD)]
 final class Signed
 {
     public function __construct(public int $status = 403)
